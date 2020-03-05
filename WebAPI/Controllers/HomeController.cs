dbf67823b86447041fc431679f86ad7c89abf713
Aldamar <danila.samusev@mail.ru>
2020-03-05 10:26:56 +0300

    Added web api

---
+++ WebAPI/Controllers/HomeController.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections.Generic;
+using HungarianAlgoritm;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebAPI.Controllers
+{
+    public class HomeController : Controller
+    {
+        [HttpPost]
+        public IActionResult Index(TaskData data)
+        {
+            var distributer = new Distributer();
+            List<DistributedGood> res = distributer.Distribute(data.Matrix, data.Goods, data.Needs);
+            
+            return Ok(res);
+        }
+    }
+}
---
+++ WebAPI/TaskData.cs
@@ -0,0 +1,9 @@
+﻿namespace WebAPI
+{
+    public class TaskData
+    {
+        public int[] Goods { get; set; }
+        public int[] Needs { get; set; }
+        public int[,] Matrix { get; set; }
+    }
+}
