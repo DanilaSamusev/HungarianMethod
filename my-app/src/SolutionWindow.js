983b9f31df72d84457ef5ad346d5fdc7c8db0c9b
Aldamar <danila.samusev@mail.ru>
2020-03-07 12:49:24 +0300

    no message

--- my-app/src/SolutionWindow.js
+++ my-app/src/SolutionWindow.js
@@ -1,156 +1,142 @@
-﻿import * as React from "react";
+﻿import React from 'react';
+import {render} from 'react-dom';
 
 export default class SolutionWindow extends React.Component {
 
     constructor(props) {
         super(props);
-
         this.state = {
+            rowCount: 2,
+            columnCount: 4,
+            matrix: null,
+            goods: null,
+            needs: null
+        };
+        this.handleInputChange = this.handleInputChange.bind(this);
+    }
 
-            rowCount: 0,
-            columnCount: 0,
-            matrix : null,
+    handleInputChange(event) {
+        let target = event.target;
+        let value = target.value;
+        let name = target.name;
+
+        let row = parseInt(name[0]);
+        let column = parseInt(name[1]);
+        let matrix = this.state.matrix;
+
+        if (isNaN(parseInt(value))) {
+            matrix[row][column] = 0;
+        } else {
+            matrix[row][column] = parseInt(value);
         }
+
+        this.setState(() => {
+            return {};
+        });
     }
 
     initializeMatrix() {
-               
-        if (this.state.rowCount > 0 && this.state.columnCount > 0) {
-
-            if (this.state.matrix != null) {
-                return;
-            }
-
-            let matrix = new Array();
-
+        if (this.state.matrix == null) {
+            let matrix = [];
             for (let row = 0; row < this.state.rowCount; row++) {
-
-                matrix[row] = new Array();
-
+                matrix[row] = [];
                 for (let column = 0; column < this.state.columnCount; column++) {
-
                     matrix[row][column] = 0;
                 }
+            }
+            this.setState(() => {
+                return {
+                    matrix: matrix
+                };
+            });
+        }
+    }
+
+    initializeGoods() {
+        if (this.state.goods == null) {
+
+            let goods = this.state.goods;
+            goods = [];
+
+            for (let i = 0; i < this.state.rowCount; i++) {
+                goods[i] = 0;
             }
 
             this.setState(() => {
                 return {
-                    matrix: matrix,
+                    goods: goods
                 };
-            })            
-        }       
+            });
+        }
+    }
+
+    initializeNeeds() {
+        if (this.state.needs == null) {
+            let needs = this.state.needs;
+            needs = [];
+
+            for (let i = 0; i < this.state.columnCount; i++) {
+                needs[i] = 0;
+            }
+
+            this.setState(() => {
+                return {
+                    needs: needs
+                };
+            });
+        }
     }
 
     render() {
 
-        initializeMatrix();
+        let matrix;
+        let goods;
+        let needs;
 
-        const matrix = this.state.matrix.map((row) =>
-            <div>{row[0]}</div>
-            )
+        if (this.state.rowCount > 0 && this.state.columnCount > 0) {
+            this.initializeMatrix();
+
+            if (this.state.matrix != null) {
+                matrix = this.state.matrix.map((row, rowId) => (
+                    <div name="row" row={row}>
+                        {row.map((elem, id) => (
+                            <input
+                                name={rowId + "" + id}
+                                onChange={this.handleInputChange}
+                                value={elem}
+                            />
+                        ))}
+                    </div>
+                ));
+            }
+        }
+
+        if (this.state.matrix != null) {
+            this.initializeGoods();
+
+            if (this.state.goods != null) {
+                goods = this.state.goods.map(good =>
+                    <div>
+                        <input value={good}/>
+                    </div>);
+            }
+        }
+
+        if (this.state.matrix != null) {
+            this.initializeGoods();
+
+            if (this.state.goods != null) {
+                goods = this.state.goods.map(good => <input value={good}/>);
+            }
+        }
+
+        return (
+            <div>
+                {matrix}
+                <div>------</div>
+                {goods}
+            </div>
+        );
     }
 }
 
-
-/*
-import React from 'react';
-import { render } from 'react-dom';
-class ParentComponent extends React.Component {
-
-  constructor(props) {
-    super(props);
-
-    this.state = {
-
-        rowCount: 0,
-        columnCount: 0,
-        matrix : null,
-        goods : null,
-        needs : null,
-    }
-
-    this.handleInputChange = this.handleInputChange.bind(this);
-}
-
-handleInputChange(event) {
-
-  let target = event.target;
-  let value = target.value;
-  let name = target.name;
-
-  let row = parseInt(name[0]);
-  let column = parseInt(name[1]);
-  let matrix = this.state.matrix
-
-  if (isNaN(parseInt(value))){
-    matrix[row][column] = 0;
-  }
-  else{
-    matrix[row][column] = parseInt(value);
-  }
-
-  this.setState(() => {
-    return {
-
-    };
-})
-}
-
-initializeMatrix() {
-
-  if (this.state.matrix == null)
-  {
-    let matrix = [];
-
-      for (let row = 0; row < this.state.rowCount; row++) {
-
-          matrix[row] = [];
-
-
-          for (let column = 0; column < this.state.columnCount; column++) {
-
-              matrix[row][column] = 0;
-          }
-      }
-
-      this.setState(() => {
-        return {
-          matrix : matrix,
-        };
-      })
-  }
-  }
-
-  render() {
-
-    let matrix;
-    let viewMatrix;
-
-    if (this.state.rowCount > 0 && this.state.columnCount > 0) {
-
-      matrix = this.initializeMatrix();
-
-      if (this.state.matrix != null){
-        viewMatrix = this.state.matrix.map((row, rowId) =>
-        <div name="row" row = {row}>
-
-             {
-               row.map((elem, id) =>
-               <input name={rowId+ "" + id} onChange={this.handleInputChange} value = {elem}>
-
-               </input>)
-             }
-
-        </div> );
-      }
-    }
-
-    return (
-      <div>
-           {viewMatrix}
-      </div>
-    )
-  }}
-
-render(<ParentComponent />, document.getElementById('root'));
- */
